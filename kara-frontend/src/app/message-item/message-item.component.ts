09b3132cb548c289e9c5fd6db103938c7ad90833
shubham sharma <shubhamsharma3lac@outlook.com>
2018-12-25 09:48:00 +0530

    updated

---
+++ kara-frontend/src/app/message-item/message-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-message-item',
+  templateUrl: './message-item.component.html',
+  styleUrls: ['./message-item.component.css']
+})
+export class MessageItemComponent implements OnInit {
+  @Input() message;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
